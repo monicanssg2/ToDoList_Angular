0167e5879ca388a7eaee67cb24303a6572207cef
monicanssg2 <monicamssg2@gmail.com>
2022-09-11 23:55:30 +0200

    first commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,68 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ITask } from '../model/task';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  todoForm !:FormGroup;
+  tasks : ITask[] = [];
+  inprogress : ITask[] = [];
+  done : ITask[] = [];
+  updateIndex !: any;
+  isEnabled : boolean = false;
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ['', Validators.required]
+    })
+  }
+  addTask(){
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done: false
+    });
+    this.todoForm.reset();
+  }
+  onEdit(item:ITask, i:number){
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updateIndex = i;
+    this.isEnabled = true;
+  }
+  updateTask(){
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEnabled = false;
+  }
+  deleteTask(i:number){
+    this.tasks.splice(i,1)
+  }
+  deleteInprogress(i:number){
+    this.inprogress.splice(i,1)
+  }
+  deleteDone(i:number){
+    this.done.splice(i,1)
+  }
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
+
